8b53645f7eefc898f18a2d034f5165ec9021374c
nicomp42 <nicholdw@ucmail.uc.edu>
2025-01-22 15:23:04 -0500

    Add project files.

---
+++ Bridge Monitor/bridgePackage/bridge.py
@@ -0,0 +1,20 @@
+# bridgeMonitor.py
+# Bill Nicholson
+# nicholdw@ucmail.uc.edu
+
+import random
+
+
+class BridgeMonitor():
+    def __init__(self):
+        pass
+    
+    def getVehicleCount(self):
+        """
+        Get the count of cars that passed over the bridge
+        @return int: The number of cars called since the last time this method was invoked. 
+        @raise an exception if the bridge is currently closed.
+        """
+        if random.randint(100, 200) % 10 == 0:
+            raise Exception("Bridge is closed")
+        return random.randint(0, 100)
